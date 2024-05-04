804a833f552259d574d11fa93c4ba09a151f0ad3
r1anl3 <levu1409@gmail.com>
2024-05-04 14:08:12 +0700

    final update

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # NT531-Project
+
 Đồ án đánh giá hiệu năng mạng máy tính UIT
 
 ## Description
 
+Enhancing monitoring system with Nagios, Prometheus and Grafana for Nginx Web server and OpenFaaS serverless function
+
 ### Objective
 
-*objective explanation here*
+- Learning about architecture and deployment of Nagios, Prometheus and Grafana
+- Comparing similarity and difference between Nagios and Prometheus
+- Evaluating compatity of those tools
+- Designing a multi-platform monitoring system
+- Testing system with load testing tools
 
 ### Tools & Technologies
 
-- Cloud - [**Google Cloud Platform**](https://cloud.google.com)
+- Cloud - [**Google Cloud Platform**](https://cloud.google.com), [**Microsoft Azure**](https://azure.microsoft.com/)
 - Infrastructure as Code (IaC) - [**Terraform**](https://www.terraform.io)
 - Monitor - [**Nagios**](https://www.nagios.org/projects/nagios-core/), [**Prometheus**](https://prometheus.io/), [**Grafana**](https://grafana.com/)
 - Function as a Service (FaaS) - [**Open FaaS**](https://www.openfaas.com/)
 - Time series databases - [**InfluxDB**](https://www.influxdata.com/)
+- Web server - [**Nginx**](https://www.nginx.com/), [**Apache 2**](https://httpd.apache.org/)
+- Load testing - [**Locust**](https://locust.io/)
 
-### Architecture *(draft)*
+### Architecture
 
-![draft system](/image/draf-arch-2.png)
+![draft system](/image/final-arch.png)
+
+### Contributors
+
+- Vu Le-Huynh-Quang, [@r1anl3](https://github.com/r1anl3)
+- Thuan Tong-Vo-Anh, [@thu4n](https://github.com/thu4n/)
--- nagios/check_nginx.py
+++ nagios/check_nginx.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-# check_nginx is a Nagios to monitor nginx statu
-# my blog: http://www.nginxs.com
-# The version is 1.0.0
-# MSN: yangzi2008@126.com
+# check_nginx is a Nagios to monitor nginx status
+# The version is 1.0.2
+# fixed by Nikolay Kandalintsev (twitter: @nicloay)
+# Based on yangzi2008@126.com from http://www.nginxs.com 
+#       which available here http://exchange.nagios.org/directory/Plugins/Web-Servers/nginx/check_nginx/details
 
 import string
 import urllib2
@@ -37,7 +38,7 @@ def usage():
             ./check_nginx -U www.nginxs.com -P /status -w 1000 -c 2000
             if don't have path and password:
             ./check_nginx -U www.nginxs.com -w 1000 -c 2000"""
- 
+
    sys.exit(3)
 
 try:
@@ -59,9 +60,9 @@ for name,value in options:
     if name in ("-p","--passwd"):
        passwd = value
     if name in ("-w","--warning"):
-       warning = int(value)
+       warning = value
     if name in ("-c","--critical"):
-       critical = int(value)
+       critical = value
 try:
    if 'path' in dir():
       req = urllib2.Request(url+path)
@@ -76,31 +77,45 @@ try:
    response = urllib2.urlopen(req)
    the_page = response.readline()
    conn = the_page.split()
-   ActiveConn = int(conn[2])
+   ActiveConn = conn[2]
    the_page1 = response.readline()
    the_page2 = response.readline()
    the_page3 = response.readline()
    response.close()
+
+   a = the_page2.split()
+   AcceptedConn = int(a[0])
+   HandledConn = int(a[1])
+   NbRequests = int(a[2])
+
+   ReqPerConn = NbRequests / HandledConn
+
    b = the_page3.split()
    reading = b[1]
    writing = b[3]
    waiting = b[5]
-   output = 'ActiveConn:%s,reading:%s,writing:%s,waiting:%s' % (ActiveConn,reading,writing,waiting)
-   perfdata = 'ActiveConn:%s,reading:%s,writing:%s,waiting:%s' % (ActiveConn,reading,writing,waiting)
+   output = 'ActiveConn:%s, reading:%s, writing:%s, waiting:%s, AcceptedConn:%s, HandledConn:%s, NbRequests:%s' % (ActiveConn,
+                                                                                                                  reading,
+                                                                                                                  writing,
+                                                                                                                  waiting,
+                                                                                                                  AcceptedConn,
+                                                                                                                  HandledConn,
+                                                                                                                  NbRequests)
+   perfdata = 'ActiveConn=%s; reading=%s; writing=%s; waiting=%s; ReqPerConn=%s' % (ActiveConn,reading,writing,waiting,ReqPerConn)
 
 except Exception:
    print "NGINX STATUS unknown: Error while getting Connection"
    sys.exit(3)
 if 'warning' in dir() and 'critical' in dir():
-   if ActiveConn >= critical:
-      print 'CRITICAL - %s' % (output)
+   if int(ActiveConn) >= int(critical):
+      print 'CRITICAL - %s|%s' % (output,perfdata)
       sys.exit(2)
-   elif ActiveConn >= warning:
-      print 'WARNING - %s' % (output)
+   elif int(ActiveConn) >= int(warning):
+      print 'WARNING - %s|%s' % (output,perfdata)
       sys.exit(1)
    else:
-      print 'OK - %s' % (output)
+      print 'OK - %s|%s' % (output,perfdata)
       sys.exit(0)
 else:
-   print 'OK - %s' % (output)
+   print 'OK - %s|%s' % (output,perfdata)
    sys.exit(0)
